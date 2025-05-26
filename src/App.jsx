d9058ab954da03d40d3a096a75ca440fc8d54eab
Komal <komalgoel9437@gmail.com>
2025-05-26 13:55:36 +0000

    Added product listing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-import "./App.css";
-import Product from "./components/Product";
+import { PageNotFound } from "components/commons";
+import Product from "components/Product";
+import { Route, Switch, NavLink, Redirect } from "react-router-dom";
 
-const App = () => <Product />;
+import ProductList from "./components/ProductList";
+
+const App = () => (
+  <>
+    <div className="flex space-x-2">
+      <NavLink exact activeClassName="underline font-bold" to="/">
+        Home
+      </NavLink>
+      <NavLink exact activeClassName="underline font-bold" to="/product">
+        Product
+      </NavLink>
+    </div>
+    <Switch>
+      <Route exact component={ProductList} path="/products" />
+      <Route exact component={Product} path="/products/:slug" />
+      <Redirect exact from="/" to="/products" />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
+);
+
 export default App;
