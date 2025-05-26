8d9df107de2284b21915ac98828a66ca88359fed
Komal <komalgoel9437@gmail.com>
2025-05-26 05:12:44 +0000

    Created product API connector file

---
+++ src/components/Product.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+
+import productsApi from "apis/products";
+import { Typography, Spinner } from "neetoui";
+import { append, isNotNil } from "ramda";
+
+import Carousel from "./Carousel";
+
+const Product = () => {
+  const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchProduct = async () => {
+    try {
+      const response = await productsApi.show();
+      console.log("API response : ", response.data);
+      setProduct(response.data);
+    } catch (error) {
+      console.log("An Error occurred : ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  const {
+    name,
+    description,
+    mrp,
+    offer_price: offerPrice,
+    image_urls: imageUrls,
+    image_url: imageUrl,
+  } = product;
+  const totalDiscounts = mrp - offerPrice;
+  const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
+
+  return (
+    <>
+      <div>
+        <Typography className="py-2 text-4xl font-semibold" style="h1">
+          {name}
+        </Typography>
+        <hr className="border-2 border-black" />
+      </div>
+      <div className="mt-16 flex gap-4">
+        <div className="w-2/5">
+          <div className="flex justify-center gap-16">
+            {isNotNil(imageUrls) ? (
+              <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
+            ) : (
+              <img alt={name} className="w-48" src={imageUrl} />
+            )}
+          </div>
+        </div>
+        <div className="w-3/5 space-y-4">
+          <Typography>{description}</Typography>
+          <Typography>MRP: {mrp}</Typography>
+          <Typography className="font-semibold">
+            Offer price: {offerPrice}
+          </Typography>
+          <Typography className="font-semibold text-green-600">
+            {discountPercentage}% off
+          </Typography>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Product;
